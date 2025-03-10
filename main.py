658b197395221fa43a8d41d0be0e9928da554f4d
Carter Hawkins <Carterhawkins93@gmail.com>
2025-03-09 22:26:51 -0400

    Have api working correctly

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hello World")
+from vtt import *
+course = get_crn('2025', Semester.SPRING, '18445')
+subjects = get_subjects()
+results = search_timetable('2025', Semester.SPRING, subject = 'MUS', code = 3314)
+print(results[0].get_name())
