47e00acf844d496854162444aa46a94e9c13653c
Carter Hawkins <Carterhawkins93@gmail.com>
2025-03-09 23:04:37 -0400

    seperated code for testing

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from vtt import *
 # can fnd course via CRN.
 course = get_crn('2025', Semester.SPRING, '18445')
-# Need the subject and course number to be able to find course.
-results = search_timetable('2025', Semester.SPRING, subject = 'MUS', code = 3314)
 
-print(results[0].get_name())
+
+def get_music_class():
+    # Need the subject and course number to be able to find course.
+    results = search_timetable('2025', Semester.SPRING, subject = 'MUS', code = 3314)
+    return results
+
+print(get_music_class()[0].get_name())
