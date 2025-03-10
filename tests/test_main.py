fe848540cb69eaa4534ce1b67812839989ecd141
Carter Hawkins <97128719+CarterH93@users.noreply.github.com>
2025-03-10 05:05:58 +0000

    updated readme

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 from scheduling_helper_code.get_music_class import *
+=======
+import CourseSchedulingApp.main
+>>>>>>> b6408a1 (updated readme)
 def test_get_music_class():
     results = get_music_class()
     assert results[0].get_subject() == 'MUS'
