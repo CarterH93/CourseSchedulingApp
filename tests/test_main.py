9765d858bba56770b391a41356d4e585402b9a68
Carter Hawkins <Carterhawkins93@gmail.com>
2025-03-09 23:18:54 -0400

    testing path

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
-from ..src.main import *
+from src import main
 def test_get_music_class():
-    results = get_music_class()
+    results = main.get_music_class()
     assert results[0].get_subject() == 'MUS'
     assert results[0].get_code() == '3314'
     assert results[0].get_name() == "Instrumental Ensemble Music"
