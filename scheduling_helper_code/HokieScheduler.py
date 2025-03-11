bbae33b0182a1667a73a9eeb7f68cd05bec0fe65
Carter Hawkins <97128719+CarterH93@users.noreply.github.com>
2025-03-11 20:46:31 +0000

    finished and tested time_do_not_interefere and schedules_do_not_interfere functions

--- scheduling_helper_code/HokieScheduler.py
+++ scheduling_helper_code/HokieScheduler.py
@@ -1,19 +1,45 @@
 from scheduling_helper_code.VTClasses import *
 class Hokie_Scheduler:
     def __init__(self):
-        self.vtclasses = Set[VTClass]()
+        self.vtclasses = set[VTClass]()
 
     def add_vtclass(self, vtclass: VTClass):
         self.vtclasses.add(vtclass)
 
-    def create_schedules(self) -> Set[Set[VTCourse]]:
+    def create_schedules(self) -> set[Set[VTCourse]]:
         #TODO #7 return lists of lists of courses for schedule
-        pass
+
+        vtclasses = list(self.vtclasses)
+
+        for VTClassIndex in range(len(vtclasses)):
+            for VTCourseIndex in range(len(vtclasses[VTClassIndex].get_courses())):
+                pass
+
+
 
     def readable_text_schedule(self) -> str:
         #TODO #8 return a string representation of the schedule
         pass
 
 
-
+    def does_not_interfere(self, VTClasses: list[VTClass], FirstVTClassIndex: int, FirstVTCourseIndex: int, SecondVTClassIndex: int, SecondVTCourseIndex: int) -> bool:
+        #TODO #9 first given course does not interfere with second given course
+        firstCourse = VTClasses[FirstVTClassIndex].get_courses()[FirstVTCourseIndex]
+        secondCourse = VTClasses[SecondVTClassIndex].get_courses()[SecondVTCourseIndex]
+  #       if firstCourse.
+        pass
     
+
+    def schedules_do_not_interfere(self, schedule1: Dict[Day, Set[Tuple[datetime.time, datetime.time]]], schedule2: Dict[Day, Set[Tuple[datetime.time, datetime.time]]]) -> bool:
+        for day in schedule1:
+            for time in schedule1[day]:
+                for time2 in schedule2[day]:
+                    if self.times_do_not_interfere(time, time2) == False:
+                        return False
+        return True
+
+    def times_do_not_interfere(self, time1: Tuple[datetime.time, datetime.time], time2: Tuple[datetime.time, datetime.time]) -> bool:
+        if time1[1] < time2[0] or (time1[0] > time2[1]):
+            return True
+        return False
+   
--- tests/test_Hokiescheduler.py
+++ tests/test_Hokiescheduler.py
@@ -1,4 +1,11 @@
 from scheduling_helper_code.HokieScheduler import *
 
-def test_HokieSchedular():
-    assert False == True
+def test_times_do_not_interfere():
+    scheduler = Hokie_Scheduler()
+    assert scheduler.times_do_not_interfere(time1=(datetime.time(11, 0), datetime.time(13, 0)), time2=(datetime.time(14, 0), datetime.time(16, 0))) == True
+    assert scheduler.times_do_not_interfere(time1=(datetime.time(15, 0), datetime.time(16, 0)), time2=(datetime.time(14, 0), datetime.time(16, 0))) == False
+
+def test_schedules_do_not_interfere():
+    scheduler = Hokie_Scheduler()
+    assert scheduler.schedules_do_not_interfere({Day.MONDAY: {(datetime.time(11, 0), datetime.time(13, 0))}, Day.TUESDAY: {(datetime.time(14, 0), datetime.time(16, 0))}}, {Day.MONDAY: {(datetime.time(15, 0), datetime.time(16, 0))}, Day.TUESDAY: {(datetime.time(14, 0), datetime.time(16, 0))}}) == False
+    assert scheduler.schedules_do_not_interfere({Day.MONDAY: {(datetime.time(11, 0), datetime.time(13, 0))}, Day.TUESDAY: {(datetime.time(14, 0), datetime.time(16, 0))}}, {Day.MONDAY: {(datetime.time(15, 0), datetime.time(16, 0))}, Day.TUESDAY: {(datetime.time(17, 0), datetime.time(18, 0))}}) == True
